e7ea0a56fc330c9380be14da65807b0b36797717
akira-shirakawa <akun32310901@yahoo.co.jp>
2021-09-01 14:57:44 +0900

    changed

--- Main/Message/delete_message.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../Class/Message.php';
-$message = new Message();
-$id2 = $_POST['id2'];
-$message->delete((int)$_POST['id']);
-header("Location:../../View/user.php?id=$id2");
-return;
